857e4a204d7d391a55b8111fe55438e0a89d6452
bradydowling <bradydowling@gmail.com>
2020-06-16 10:01:44 -0400

    Improve chart

--- src/index.js
+++ src/index.js
@@ -2,19 +2,22 @@ import * as d3 from 'd3';
 
 const data = [4, 8, 15, 16, 23, 36];
 
-const width = 420;
+const height = 420;
+const barWidth = 30;
+const barSpacing = 1;
+const labelPadding = 3;
 
-const x = d3.scaleLinear()
-    .domain([0, d3.max(data)])
-    .range([0, width]);
-
-const y = d3.scaleBand()
+const x = d3.scaleBand()
     .domain(d3.range(data.length))
-    .range([0, 20 * data.length]);
+    .range([0, barWidth * data.length]);
+
+const y = d3.scaleLinear()
+    .domain([0, d3.max(data)])
+    .range([0, height]);
 
 const svg = d3.create('svg')
-    .attr('width', width)
-    .attr('height', y.range()[1])
+    .attr('width', x.range()[1])
+    .attr('height', height)
     .attr('font-family', 'sans-serif')
     .attr('font-size', 10)
     .attr('text-anchor', 'end');
@@ -22,18 +25,18 @@ const svg = d3.create('svg')
 const bar = svg.selectAll('g')
     .data(data)
     .join('g')
-        .attr('transform', (d, i) => `translate(0,${y(i)})`);
+        .attr('transform', (d, i) => `translate(${x(i)},${height - y(d)})`);
 
 bar.append('rect')
     .attr('fill', 'steelblue')
-    .attr('width', x)
-    .attr('height', y.bandwidth() - 1);
+    .attr('width', x.bandwidth() - barSpacing)
+    .attr('height', y);
 
 bar.append('text')
     .attr('fill', 'white')
-    .attr('x', d => x(d) - 3)
-    .attr('y', y.bandwidth() / 2)
-    .attr('dy', '0.35em')
-    .attr(d => d);
+    .attr('x', x.bandwidth() / 2)
+    .attr('y', d => y(d) - labelPadding)
+    .attr('dx', '0.35em')
+    .text(d => d);
 
 document.querySelector('body').appendChild(svg.node());
